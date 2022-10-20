f43733e1ca7b8463a27029706025a93cc00b37cd
Carlos Marti <carlos199m1@gmail.com>
2022-10-20 14:57:57 -0400

    GP models working with reasonable predictions on basic data

--- epik/src/model.py
+++ epik/src/model.py
@@ -1,16 +1,23 @@
+from tqdm import tqdm
 import numpy as np
 import torch
 import gpytorch
 
 from gpytorch.kernels.multi_device_kernel import MultiDeviceKernel
-from tqdm import tqdm
+from gpytorch.mlls.exact_marginal_log_likelihood import ExactMarginalLogLikelihood
+from gpytorch.likelihoods.gaussian_likelihood import (FixedNoiseGaussianLikelihood,
+                                                      GaussianLikelihood)
 
 
 class GPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, kernel, likelihood,
-                 output_device=None, n_devices=None):
+                 output_device=None, n_devices=None, train_mean=False):
         super(GPModel, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.ConstantMean()
+        
+        if train_mean:
+            self.mean_module = gpytorch.means.ConstantMean()
+        else:
+            self.mean_module = gpytorch.means.ZeroMean()
         
         if output_device is None:
             self.covar_module = kernel
@@ -26,11 +33,15 @@ class GPModel(gpytorch.models.ExactGP):
 
 
 class EpiK(object):
-    def __init__(self, kernel, likelihood, output_device=None, n_devices=None):
+    def __init__(self, kernel, likelihood_type='Gaussian',
+                 output_device=None, n_devices=1,
+                 dtype=torch.float32, train_mean=False):
         self.kernel = kernel
-        self.likelihood = likelihood
+        self.likelihood_type = likelihood_type
         self.output_device = output_device
         self.n_devices = n_devices
+        self.dtype = dtype
+        self.train_mean = train_mean
     
     def seq_to_one_hot(self, X):
         m = np.array([[a for a in x] for x in X])
@@ -39,33 +50,83 @@ class EpiK(object):
             c = m[:, i]
             alleles = np.unique(c)
             for allele in alleles:
-                onehot.append(torch.tensor(c == allele, dtype=torch.float32))
+                onehot.append(self.get_tensor(c == allele))
         onehot = torch.stack(onehot, 1)
         return(onehot)
+
+    def to_device(self, tensor):
+        if self.output_device is not None:
+            tensor = tensor.to(self.output_device)
+        return(tensor)
+
+    def get_tensor(self, ndarray):
+        if not torch.is_tensor(ndarray):
+            ndarray = torch.tensor(ndarray, dtype=self.dtype)
+        return(self.to_device(ndarray))
     
-    def fit(self, X, y, n_iter=100):
+    def set_likelihood(self, y_var=None):
+        if self.likelihood_type == 'Gaussian':
+            if y_var is not None:
+                likelihood = FixedNoiseGaussianLikelihood(noise=self.get_tensor(y_var),
+                                                          learn_additional_noise=False)
+            else:
+                likelihood = GaussianLikelihood()
+        else:
+            msg = 'Only Gaussian likelihood is allowed so far'
+            raise ValueError(msg)
+        
+        self.likelihood = self.to_device(likelihood)
+    
+    def report_progress(self, pbar, loss):
+        if self.output_device is not None:
+            loss = loss.cpu()
+        report_dict = {'loss': '{:.3f}'.format(loss.detach().numpy())}
+        if hasattr(self.model.covar_module, 'log_lda'):
+            lambdas = self.model.covar_module.log_lda
+            lambdas_text = ['{:.2f}'.format(np.exp(l)) for l in lambdas.detach().numpy()]
+            lambdas_text = '[{}]'.format(', '.join(lambdas_text))
+            report_dict['lambdas'] = lambdas_text
+        
+        if self.model.covar_module.lengthscale is not None:
+            v = self.model.covar_module.lengthscale.detach().numpy()
+            report_dict['lengthscale'] = '{:.2f}'.format(v[0][0])
+        elif hasattr(self.model.covar_module, 'base_kernel'):
+            if self.model.covar_module.base_kernel.lengthscale is not None:
+                v = self.model.covar_module.base_kernel.lengthscale.detach().numpy()
+                report_dict['lengthscale'] = '{:.2f}'.format(v[0][0])
+        pbar.set_postfix(report_dict)
+    
+    def fit(self, X, y, y_var=None, n_iter=100, learning_rate=0.1):
         x = self.seq_to_one_hot(X)
-        model = GPModel(x, y, self.kernel, self.likelihood,
-                        output_device=self.output_device,
-                        n_devices=self.n_devices)
-        if self.output_device:
-            model = model.to(self.output_device)
-        self.model = model 
+        y = self.get_tensor(y)
+        self.set_likelihood(y_var=y_var)
+        self.model = self.to_device(GPModel(x, y, self.kernel, self.likelihood,
+                                            output_device=self.output_device,
+                                            n_devices=self.n_devices,
+                                            train_mean=self.train_mean)) 
+
         self.model.train()
         self.likelihood.train()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
+        mll = ExactMarginalLogLikelihood(self.likelihood, self.model)
         
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1)
-        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self.likelihood, self.model)
-        
-        for _ in tqdm(range(n_iter)):
+        pbar = tqdm(range(n_iter), desc='Iterations')
+        for _ in pbar:
             optimizer.zero_grad()
             output = self.model(x)
             loss = -mll(output, y)
             loss.backward()
             optimizer.step()
+            
+            self.report_progress(pbar, loss)
     
     def predict(self, X):
         x = self.seq_to_one_hot(X)
-        f_preds = self.model(x)
-        return(f_preds.mean, f_preds.variance)
+        self.model.eval()
+        self.likelihood.eval()
+        
+        with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            f_preds = self.model(x)
+        # TODO: error when asking for variance: f_preds.variance
+        return(f_preds.mean)
         
--- epik/test/test_model.py
+++ epik/test/test_model.py
@@ -7,40 +7,79 @@ import torch
 
 from os.path import join
 
-from gpytorch.likelihoods.gaussian_likelihood import GaussianLikelihood
+from gpytorch.kernels.rbf_kernel import RBFKernel
+from gpytorch.kernels.scale_kernel import ScaleKernel
 
-from epik.src.kernel import SkewedVCKernel
 from epik.src.settings import TEST_DATA_DIR
+from epik.src.kernel import SkewedVCKernel
 from epik.src.model import EpiK
 
 
-
 class ModelsTests(unittest.TestCase):
-    def test_epik(self):
-        data = pd.read_csv(join(TEST_DATA_DIR, 'smn1data.csv'),
-                           header=None, index_col=0, names=['m', 'std'])
-        
+    def test_one_hot_encoding(self):
         kernel = SkewedVCKernel(n_alleles=2, seq_length=2)
-        likelihood = GaussianLikelihood()
-        
-        model = EpiK(kernel, likelihood)
-        
+        model = EpiK(kernel, likelihood_type='Gaussian')
         X = np.array(['AA', 'AB', 'BA', 'BB'])
-        y = torch.tensor([0.2, 1.1, 0.5, 1.5])
-        
-        # Ensure proper 1-hot encoding
         x = model.seq_to_one_hot(X).numpy()
         onehot = np.array([[1, 0, 1, 0],
                            [1, 0, 0, 1],
                            [0, 1, 1, 0],
                            [0, 1, 0, 1.]])
         assert(np.allclose(x - onehot, 0))
-
-        with torch.autograd.set_detect_anomaly(True):
-            model.fit(X, y)
         
+    def test_epik_basic(self):
+        kernel = SkewedVCKernel(n_alleles=2, seq_length=2)
+        model = EpiK(kernel, likelihood_type='Gaussian')
         
+        X = np.array(['AA', 'AB', 'BA', 'BB'])
+        y = torch.tensor([0.2, 1.1, 0.5, 1.5])
+        model.fit(X, y, n_iter=100)
+        ypred = model.predict(X)
+        assert(ypred.shape[0] == 4)
+    
+    def test_epik_basic_gpu(self):
+        output_device = torch.device('cuda:0') 
+        kernel = SkewedVCKernel(n_alleles=2, seq_length=2)
+        model = EpiK(kernel, likelihood_type='Gaussian',
+                     output_device=output_device)
         
+        X = np.array(['AA', 'AB', 'BA', 'BB'])
+        y = torch.tensor([0.2, 1.1, 0.5, 1.5])
+        model.fit(X, y, n_iter=100)
+        ypred = model.predict(X)
+        assert(ypred.shape[0] == 4)
+        
+    def test_epik_basic_RBF(self):
+        kernel = ScaleKernel(RBFKernel())
+        model = EpiK(kernel, likelihood_type='Gaussian', train_mean=True)
+        
+        X = np.array(['AA', 'AB', 'BA', 'BB'])
+        y = torch.tensor([0.2, 1.1, 0.5, 1.5])
+        model.fit(X, y, n_iter=200)
+        ypred = model.predict(X)
+        print(ypred)
+    
+    def test_epik_smn1_RBFkernel(self):
+        data = pd.read_csv(join(TEST_DATA_DIR, 'smn1data.csv'),
+                           header=None, index_col=0, names=['m', 'std'])
+        data = data.loc[[x[3] == 'U' for x in data.index], :]
+        data.index = [x[:3] + x[4:] for x in data.index]
+        data = data.loc[np.random.choice(data.index, size=1000), :]
+        
+        X = data.index.values
+        y = data['m'].values
+        y_var = (data['std'] ** 2).values
+        
+        # kernel = SkewedVCKernel(n_alleles=4, seq_length=7)
+        output_device = None
+        # output_device = torch.device('cuda:0')
+        
+        kernel = ScaleKernel(RBFKernel())
+        model = EpiK(kernel, likelihood_type='Gaussian', train_mean=True,
+                     output_device=output_device)
+        model.fit(X, y, y_var=y_var, n_iter=1000)
+        ypred = model.predict(X)
+        print(ypred)
         
         
 if __name__ == '__main__':
