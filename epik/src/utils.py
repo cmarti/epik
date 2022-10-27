d57d533945694688185464f8838fab27cf8b1cfa
Carlos Marti <carlos199m1@gmail.com>
2022-10-27 13:26:48 -0400

    alleles argument missing added

--- epik/src/utils.py
+++ epik/src/utils.py
@@ -9,12 +9,12 @@ def get_alleles(c, alleles=None):
             return(np.unique(c))
 
         
-def seq_to_one_hot(X):
+def seq_to_one_hot(X, alleles=None):
     m = np.array([[a for a in x] for x in X])
     onehot = []
     for i in range(m.shape[1]):
         c = m[:, i]
-        for allele in get_alleles(c):
+        for allele in get_alleles(c, alleles=alleles):
             onehot.append(get_tensor(c == allele))
     onehot = torch.stack(onehot, 1)
     return(onehot)
