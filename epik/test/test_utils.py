868b701a15c2876f9a22a3c5b6a04dbc7e6a7f7b
Carlos Marti <carlos199m1@gmail.com>
2022-11-03 11:46:56 -0400

    error in test with old import fixed

--- epik/test/test_utils.py
+++ epik/test/test_utils.py
@@ -2,9 +2,8 @@
 import unittest
 
 import numpy as np
-import torch
 
-from epik.src.utils import seq_to_one_hot, get_theta_to_log_lda_matrix
+from epik.src.utils import seq_to_one_hot
 
 
 class UtilsTests(unittest.TestCase):
@@ -17,11 +16,6 @@ class UtilsTests(unittest.TestCase):
                            [0, 1, 0, 1.]])
         assert(np.allclose(x - onehot, 0))
     
-    def test_theta_to_log_lda_matrix(self):
-        m = get_theta_to_log_lda_matrix(l=7)
-        print(m.numpy().astype(int))
-        
-        
         
 if __name__ == '__main__':
     import sys;sys.argv = ['', 'UtilsTests']
