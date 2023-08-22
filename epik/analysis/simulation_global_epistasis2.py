685fe37df3ae25b9490e23897b589354a824b2f8
Carlos Marti <carlos199m1@gmail.com>
2023-08-22 15:54:58 -0400

    script to analyze extrapolation on crater landscape completed

--- epik/analysis/simulation_global_epistasis2.py
+++ epik/analysis/simulation_global_epistasis2.py
@@ -13,8 +13,11 @@ from scipy.stats import pearsonr
 
 from epik.src.utils import seq_to_one_hot, get_tensor
 from epik.src.model import EpiK
-from epik.src.kernel import GeneralizedSiteProductKernel
+from epik.src.kernel import GeneralizedSiteProductKernel, VCKernel
 from epik.src.priors import AllelesProbPrior, RhosPrior
+from build.lib.epik.src.priors import LambdasFlatPrior
+from gpytorch.kernels.scale_kernel import ScaleKernel
+from gpytorch.kernels.rbf_kernel import RBFKernel
 
 
 
@@ -45,8 +48,8 @@ class GlobalEpistasisModel(object):
     def simulate(self, sigma2=0):
         seqs = self.get_sequences()
         X = self.seqs_to_X(seqs)
-        phi = self.X_to_phi(X)
-        y_true = self.phi_to_y(phi).flatten()
+        phi = self.X_to_phi(X).flatten()
+        y_true = self.phi_to_y(phi)
         y, y_var = self.add_noise(y_true, sigma2=sigma2)
         return(X, phi, y_true, y, y_var)
     
@@ -85,20 +88,25 @@ class GlobalEpistasisModel(object):
 
 
 class CraterLandscape(GlobalEpistasisModel):
-    def __init__(self, l=16, d0=3, lengthscale=1):
+    def __init__(self, l=16, dg=1, s=1, rho_on=6, rho_off=1):
         self.l = l
         self.alleles = ['A', 'B']
         self.alpha = 2
         
-        self.d0 = d0
-        self.lengthscale = lengthscale
+        self.dg = dg
+        self.s = s
+        self.rho_on = rho_on
+        self.rho_off = rho_off
+        
         self.seq0 = 'A' * self.l
         self.X0 = seq_to_one_hot([self.seq0], alleles=self.alleles)
         self.dtype = torch.float32
         self.phi_range = (0, self.l+1)
     
     def phi_to_y(self, phi):
-        return(np.exp(-(phi - self.d0)**2 / self.lengthscale))
+        f = self.s / (1 + np.exp(self.dg * (phi - self.rho_on)))
+        f -= self.s / (1 + np.exp(self.dg * (phi - self.rho_off))) 
+        return(f)
 
     def X_to_phi(self, X):
         return(self.l - X.matmul(self.X0.T))
@@ -117,7 +125,6 @@ class SigmoidLandscape(GlobalEpistasisModel):
         self.lower = lower
         
     def X_to_phi(self, X):
-        print(self.seq0, self.X0.shape)
         return(self.l - X.matmul(self.X0.T))    
     
     def get_thetas(self, fixed=False):
@@ -136,16 +143,26 @@ class SigmoidLandscape(GlobalEpistasisModel):
 
 
 class GPmodel(object):
-    def __init__(self, l, a, train_p=False, sites_equal=True, track_progress=False):
+    def __init__(self, l, a, train_p=False, sites_equal=True, track_progress=False,
+                 kernel='rho'):
         self.l = l
         self.a = a
-        rhos_prior = RhosPrior(seq_length=l, n_alleles=a,
-                               sites_equal=sites_equal, train=True)
-        p_prior = AllelesProbPrior(seq_length=l, n_alleles=a, dummy_allele=False,
-                                   sites_equal=False, train=train_p)
-        
-        kernel = GeneralizedSiteProductKernel(n_alleles=a, seq_length=l,
-                                              rho_prior=rhos_prior, p_prior=p_prior)
+        if kernel == 'rho':
+            rhos_prior = RhosPrior(seq_length=l, n_alleles=a,
+                                   sites_equal=sites_equal, train=True)
+            p_prior = AllelesProbPrior(seq_length=l, n_alleles=a, dummy_allele=False,
+                                       sites_equal=True, train=train_p)
+            kernel = GeneralizedSiteProductKernel(n_alleles=a, seq_length=l,
+                                                  rho_prior=rhos_prior, p_prior=p_prior)
+        elif kernel == 'VC':
+            lambdas_prior = LambdasFlatPrior(seq_length=l)
+            kernel = VCKernel(seq_length=l, n_alleles=a, lambdas_prior=lambdas_prior)
+        elif kernel == 'RBF':
+            kernel = RBFKernel()
+        else:
+            msg = 'Kernel not allowed: {}'.format(kernel)
+            raise ValueError(msg)
+            
         self.model = EpiK(kernel, likelihood_type='Gaussian',
                           track_progress=track_progress)
     
@@ -163,8 +180,11 @@ class GPmodel(object):
     def train(self, train_x, train_y, train_y_var, n=100):
         self.model.set_data(train_x, train_y, train_y_var)
         self.model.fit(n_iter=n)
-        params = self.model.kernel.get_params()
-        params = {k: v.detach().numpy() for k, v in params.items()}
+        try:
+            params = self.model.kernel.get_params()
+            params = {k: v.detach().numpy() for k, v in params.items()}
+        except:
+            return({})
         return(params)
 
     def evaluate_predictions(self, test_y, test_y_pred):
@@ -185,13 +205,26 @@ class GPmodel(object):
 
 def plot_predictions(axes, phi_true, y_true, xs, ys, 
                      xlabel=r'Latent phenotype $\phi$',
-                     ylabel='', title='', xlims=(0, 14)):
-    axes.scatter(phi_true, y_true, alpha=0.2, s=10, c='black')
-    axes.plot(xs, ys, c='grey', lw=1)
+                     ylabel='', title='', r2=None,
+                     train_y_mean=None):
+    axes.scatter(phi_true, y_true, alpha=0.2, s=10, c='black',
+                 label='data')
+    axes.plot(xs, ys, c='grey', lw=1, label='landscape')
     axes.set(xlabel=xlabel,
-             ylabel=ylabel,
-             xlim=xlims, title=title)
+             ylabel=ylabel)
     axes.grid(alpha=0.1)
+    if r2 is not None:
+        axes.text(0.05, 0.95, '$R^{2}$' + '={:.2f}'.format(r2),
+                  transform=axes.transAxes, ha='left', va='top')
+    
+    if title:
+        axes.text(0.05, 0.05, title,
+                  transform=axes.transAxes, ha='left', va='bottom')
+    
+    if train_y_mean is not None:
+        axes.plot((xs[0], xs[-1]), (train_y_mean, train_y_mean),
+                  lw=1, linestyle='--', c='lightgrey',
+                  label='empirical mean')
 
 
 def plot_hist(axes, phi, bins, title=''):
@@ -201,75 +234,90 @@ def plot_hist(axes, phi, bins, title=''):
 
 
 if __name__ == '__main__':
-    l, alpha = 11, 2
-    pmut = 0.1
+    l, alpha = 10, 2
+    uniform = False
+    pmut = 0.02
     n = 500
     ptrain = 0.8
 
-    lengthscale = 3
     a = -3
     b = 3
+    c = 3
     lower = 0
     upper = 1
     sigma2 = 0
 
     # Generate data
-    landscape = CraterLandscape(l=l, d0=3, lengthscale=lengthscale)
+    landscape = CraterLandscape(l=l)
     X, phi, y_true, y, y_var = landscape.simulate(sigma2)
     xs, ys = landscape.get_xs_ys()
     
     
     # Split data
     ps = [0.01, 0.1, 0.5, 0.8]
-    fig, subplots = plt.subplots(3, len(ps), figsize=(4*len(ps), 9),
-                                 sharex=True, sharey=False)
+    fig, subplots = plt.subplots(3, len(ps), figsize=(3.5*len(ps), 9),
+                                 sharex=True, sharey='row')
     
     for p, axes in zip(ps, subplots.T):
-        splits = landscape.split_data([X, y, y_var, phi], p=p, uniform=False, pmut=pmut)
+        splits = landscape.split_data([X, y, y_var, phi], p=p, uniform=uniform, pmut=pmut)
         train_x, train_y, train_y_var, train_phi = splits[:4]
         test_x, test_y, test_y_var, test_phi = splits[4:]
+        train_y_mean = train_y.mean()
         
         # Plot distance distribution
         plot_hist(axes[0], train_phi, bins=np.arange(l+1),
-                  title=r'Uniform $\rho$' + ' ({:.0f}% training)'.format(p*100),)
+                  title='{:.0f}% training data'.format(p*100),)
         
         # Train fixed rho
         model = GPmodel(l, alpha, train_p=False, sites_equal=True,
-                        track_progress=False)
+                        track_progress=False, kernel='rho')
         params = model.train(train_x, train_y, train_y_var, n=50)
         test_y_pred = model.predict(train_x, train_y, train_y_var, test_x)
-        
-        print(train_x.shape, test_x.shape, params['rho'][0], model.evaluate_predictions(test_y, test_y_pred))
-        
+        evals = model.evaluate_predictions(test_y, test_y_pred)
         plot_predictions(axes[1], test_phi, test_y_pred, xs, ys,
-                         xlabel='', ylabel='y', title='')
+                         xlabel='', title=r'$\rho$ kernel',
+                         r2=evals['r2'], train_y_mean=train_y_mean)
+        ylim = axes[1].get_ylim()
+#         
+#         # RBF kernel
+#         model = GPmodel(l, alpha, train_p=False, sites_equal=True,
+#                         track_progress=False, kernel='RBF')
+#         params = model.train(train_x, train_y, train_y_var, n=50)
+#         test_y_pred = model.predict(train_x, train_y, train_y_var, test_x)
+#         evals = model.evaluate_predictions(test_y, test_y_pred)
+#         plot_predictions(axes[2], test_phi, test_y_pred, xs, ys,
+#                          xlabel='', title='RBF kernel',
+#                          r2=evals['r2'], train_y_mean=train_y_mean)
         
-        # # Change mean
-        # model = GPmodel(l, alpha, train_p=False, sites_equal=True)
-        # params = model.train(train_x, train_y-1, train_y_var, n=100)
-        # test_y_pred = model.predict(train_x, train_y-1, train_y_var, test_x)
-        # plot_predictions(axes[2], test_phi, test_y_pred, xs, ys-1,
-        #                  title='', xlabel='Hamming distance from WT',
-        #                  ylabel='y')
+        # VC kernel
+        model = GPmodel(l, alpha, kernel='VC', track_progress=False)
+        params = model.train(train_x, train_y, train_y_var, n=50)
+        test_y_pred = model.predict(train_x, train_y, train_y_var, test_x)
+        evals = model.evaluate_predictions(test_y, test_y_pred)
+        plot_predictions(axes[2], test_phi, test_y_pred, xs, ys,
+                         title=r'VC kernel', 
+                         xlabel='Hamming distance from WT',
+                         r2=evals['r2'], train_y_mean=train_y_mean)
+        axes[2].set_ylim(ylim)
+        print(train_x.shape, test_x.shape, params['lambdas'])
         
+#         # Change mean
+#         model = GPmodel(l, alpha, train_p=False, sites_equal=True)
+#         params = model.train(train_x, train_y-c, train_y_var, n=50)
+#         test_y_pred = model.predict(train_x, train_y-c, train_y_var, test_x)
+#         evals = model.evaluate_predictions(test_y, test_y_pred)
+#         plot_predictions(axes[2], test_phi, test_y_pred, xs, ys-c,
+#                          title='', xlabel='Hamming distance from WT',
+#                          r2=evals['r2'])
         
+    subplots[1][0].set_ylabel('y')
+    subplots[2][0].set_ylabel('y')
     
-    #
-    # record = evaluate_predictions(ptrain, test_y_pred, test_y)
-    # record['rho'] = params['rho']
-    # print(record)
-    # print(params0['rho'])
-    #
-    # axes = subplots[1]
-    # plot_true_predictions(axes, phi_train, train_y_pred, a=a, b=b,
-    #                       lower=0, upper=1)
-    #
-    # axes = subplots[2]
-    # plot_true_predictions(axes, phi_test, test_y_pred, a=a, b=b,
-    #                       lower=0, upper=1)
     
     fig.tight_layout()
-    fig.savefig('global_epistasis.png', dpi=300)
+    
+    sampling = 'uniform' if uniform else 'mutation'
+    fig.savefig('global_epistasis.l{}.{}.png'.format(l, sampling), dpi=300)
     
     exit()
     
