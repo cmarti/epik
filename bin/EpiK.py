0b47a49ab7cd33c0717fbb49ea56bc0b36bc77d8
Carlos Marti <carlos199m1@gmail.com>
2023-10-20 15:57:29 -0400

    binary update

--- bin/EpiK.py
+++ bin/EpiK.py
@@ -6,22 +6,48 @@ import pandas as pd
 import torch
 
 from gpytorch.settings import max_cg_iterations
-from gpytorch.kernels.rbf_kernel import RBFKernel
-from gpytorch.kernels.scale_kernel import ScaleKernel
-from gpytorch.kernels.matern_kernel import MaternKernel
-from gpytorch.kernels.rq_kernel import RQKernel
-from gpytorch.kernels.linear_kernel import LinearKernel
+from gpytorch.kernels import (RBFKernel, ScaleKernel, MaternKernel,
+                              RQKernel, LinearKernel)
 
-from epik.src.kernel import (SkewedVCKernel, VCKernel,
-                             GeneralizedSiteProductKernel)
+from epik.src.kernel import (ConnectednessKernel, VarianceComponentKernel,
+                             DeltaPKernel, RhoPiKernel)
 from epik.src.model import EpiK
-from epik.src.priors import (LambdasExpDecayPrior, AllelesProbPrior,
-                             LambdasFlatPrior, LambdasMonotonicDecayPrior,
-                             LambdasDeltaPrior, RhosPrior)
-from epik.src.utils import (LogTrack, guess_space_configuration, seq_to_one_hot,
-                            get_tensor)
+from epik.src.utils import LogTrack, guess_space_configuration, seq_to_one_hot
 from epik.src.plot import plot_training_history
 
+
+def select_kernel(kernel, n_alleles, seq_length, dtype, P):
+    if kernel == 'RBF':
+        kernel = ScaleKernel(RBFKernel())
+    elif kernel == 'ARD':
+        kernel = ScaleKernel(RBFKernel(ard_num_dims=n_alleles * seq_length))
+    elif kernel == 'matern':
+        kernel = ScaleKernel(MaternKernel())
+    elif kernel == 'RQ':
+        kernel = ScaleKernel(RQKernel())
+    elif kernel == 'linear':
+        kernel = ScaleKernel(LinearKernel())
+    else:
+        if kernel == 'Connectedness' or kernel == 'Rho':
+            kernel = ConnectednessKernel(n_alleles, seq_length, dtype=dtype)
+        elif kernel == 'RhoPi':
+            kernel = RhoPiKernel(n_alleles, seq_length, dtype=dtype)
+        elif kernel == 'VC':
+            kernel = VarianceComponentKernel(n_alleles, seq_length, dtype=dtype)
+        elif kernel == 'DP':
+            kernel = DeltaPKernel(n_alleles, seq_length, P=P, dtype=dtype)
+        else:
+            msg = 'Unknown kernel provided: {}'.format(kernel)
+            raise ValueError(msg)
+    return(kernel)
+
+
+def read_test_sequences(test_fpath):
+    seqs = np.array([])
+    if test_fpath is not None:
+        seqs = np.array([line.strip().strip('"') for line in open(test_fpath)])
+    return(seqs)
+        
         
 def main():
     description = 'Runs Gaussian-Process regression on sequence space using data'
@@ -36,18 +62,13 @@ def main():
 
     options_group = parser.add_argument_group('Kernel options')
     options_group.add_argument('-k', '--kernel', default='VC',
-                               help='Kernel function to use (VC, sVC, SiteProduct, GeneralizedSiteProduct, Diploid, RBF, RQ, matern, linear)')
-    options_group.add_argument('--q', default=None, type=float,
-                               help='Probability of leaving under the discrete time chain in sVC prior (l-1)/l')
-    options_group.add_argument('--train_p', action='store_true', default=False,
-                               help='Train p parameters from the kernel')
-    options_group.add_argument('--dummy_allele', action='store_true', default=False,
-                               help='Add dummy allele to each site')
-    options_group.add_argument('--lprior', default=None,
-                               help='Type of prior on log(lambdas) {None, delta, monotonic_decay, 2nd_order_diff}')
+                               help='Kernel function to use (VC, DP, Connectedness, RhoPi, RBF, ARD, RQ, matern, linear)')
+#     options_group.add_argument('--q', default=None, type=float,
+#                                help='Probability of leaving under the discrete time chain in sVC prior (l-1)/l')
     options_group.add_argument('-P', '--P', default=2, type=int,
                                help='P for Delta(P) prior (2)')
-    
+    options_group.add_argument('--params', default=None,
+                               help='Model parameters to use for predictions')
     
     comp_group = parser.add_argument_group('Computational options')
     comp_group.add_argument('--gpu', default=False, action='store_true',
@@ -57,9 +78,7 @@ def main():
     comp_group.add_argument('-m', '--n_devices', default=1, type=int,
                             help='Number of GPUs to use (1)')
     comp_group.add_argument('-s', '--partition_size', default=0, type=int,
-                            help='Use kernel partitioning on GPU of this size')
-    comp_group.add_argument('-t', '--preconditioner_size', default=0, type=int,
-                            help='Size of the preconditioner for CG-solve Kx=y')
+                            help='Use kernel partitioning of this size')
     comp_group.add_argument('-n', '--n_iter', default=200, type=int,
                             help='Number of iterations for optimization (200)')
     comp_group.add_argument('-r', '--learning_rate', default=0.1, type=float,
@@ -75,18 +94,15 @@ def main():
     data_fpath = parsed_args.data
     
     kernel = parsed_args.kernel
-    q = parsed_args.q
-    lambdas_prior = parsed_args.lprior
+#     q = parsed_args.q
     P = parsed_args.P
-    train_p = parsed_args.train_p
-    dummy_allele = parsed_args.dummy_allele
+    params_fpath = parsed_args.params
     
     gpu = parsed_args.gpu
     n_devices = parsed_args.n_devices
     n_iter = parsed_args.n_iter
     learning_rate = parsed_args.learning_rate
     partition_size = parsed_args.partition_size
-    preconditioner_size = parsed_args.preconditioner_size
     use_float64 = parsed_args.use_float64
 
     pred_fpath = parsed_args.pred
@@ -100,14 +116,10 @@ def main():
     
     # Get processed data
     seqs = data.index.values
-    if pred_fpath is None:
-        pred_seqs = np.array([])
-    else:
-        pred_seqs = np.array([line.strip().strip('"')
-                              for line in open(pred_fpath)]) 
-    
-    config = guess_space_configuration(np.append(seqs, pred_seqs))
+    test_seqs = read_test_sequences(pred_fpath)
+    config = guess_space_configuration(np.append(seqs, test_seqs))
     alleles = np.unique(np.hstack(config['alphabet']))
+    n_alleles = alleles.shape[0]
     X = seq_to_one_hot(seqs, alleles=alleles)
     y = data.values[:, 0]
     if data.shape[1] > 1:
@@ -115,128 +127,46 @@ def main():
         y_var[y_var < 0.0001] = 0.0001
     else:
         y_var =  None
-    
-    if use_float64:
-        X = get_tensor(X, torch.float64)
-        y = get_tensor(y, torch.float64)
-        if y_var is not None:
-            y_var = get_tensor(y_var, torch.float64)
-        dtype = torch.float64
-    else:
-        dtype = torch.float32
+    dtype = torch.float64 if use_float64 else torch.float32
       
     # Get kernel
     log.write('Selected {} kernel'.format(kernel))
-    if kernel == 'RBF':
-        kernel = ScaleKernel(RBFKernel())
-    elif kernel == 'ARD':
-        kernel = ScaleKernel(RBFKernel(ard_num_dims=X.shape[1]))
-    elif kernel == 'matern':
-        kernel = ScaleKernel(MaternKernel())
-    elif kernel == 'RQ':
-        kernel = ScaleKernel(RQKernel())
-    elif kernel == 'linear':
-        kernel = ScaleKernel(LinearKernel())
-    else:
-        n_alleles, seq_length = np.max(config['n_alleles']), config['length']
-        p_prior = AllelesProbPrior(seq_length=seq_length, n_alleles=n_alleles,
-                                   train=train_p, dtype=dtype, dummy_allele=dummy_allele)
-        
-        log.write('Use {} prior on lambdas'.format(lambdas_prior))
-        if lambdas_prior is None:
-            lambdas_prior = LambdasFlatPrior(seq_length=seq_length, dtype=dtype)
-        elif lambdas_prior == 'delta':
-            lambdas_prior = LambdasDeltaPrior(seq_length, n_alleles, P=P, dtype=dtype)
-        elif lambdas_prior == '2nd_order_diff':
-            lambdas_prior = LambdasExpDecayPrior(seq_length=seq_length, dtype=dtype)
-        elif lambdas_prior == 'monotonic_decay':
-            lambdas_prior = LambdasMonotonicDecayPrior(seq_length=seq_length, dtype=dtype)
-        else:
-            msg = 'Lambdas prior unknown: {}'.format(lambdas_prior)
-            raise ValueError(msg)    
-        
-        if kernel == 'GeneralizedSiteProduct':
-            rho_prior = RhosPrior(seq_length=seq_length, n_alleles=n_alleles)
-            kernel = GeneralizedSiteProductKernel(n_alleles=n_alleles, seq_length=seq_length,
-                                                  p_prior=p_prior, rho_prior=rho_prior,
-                                                  dtype=dtype)
-        elif kernel == 'VC':
-            kernel = VCKernel(n_alleles=n_alleles, seq_length=seq_length,
-                              lambdas_prior=lambdas_prior, dtype=dtype)
-        elif kernel == 'sVC':
-            kernel = SkewedVCKernel(n_alleles=n_alleles, seq_length=seq_length, 
-                                    lambdas_prior=lambdas_prior, p_prior=p_prior,
-                                    q=q, dtype=dtype)
-        elif kernel == 'Diploid':
-            msg = 'Diploid kernel Not implemented yet'
-            raise ValueError(msg)
-        else:
-            msg = 'Unknown kernel provided: {}'.format(kernel)
-            raise ValueError(msg)
+    kernel = select_kernel(kernel, n_alleles, config['seq_length'],
+                           dtype=dtype, P=P)
     
     # Create model
     max_cg_iterations(3000)
     log.write('Building model for Gaussian Process regression')
     output_device = torch.device('cuda') if gpu else None
-    model = EpiK(kernel, likelihood_type='Gaussian', dtype=dtype,
+    model = EpiK(kernel, dtype=dtype,
                  output_device=output_device, n_devices=n_devices,
-                 partition_size=partition_size, learning_rate=learning_rate,
-                 preconditioner_size=preconditioner_size)
+                 partition_size=partition_size, learning_rate=learning_rate)
     model.set_data(X, y, y_var=y_var)
 
     # Fit by evidence maximization
-    log.write('Estimate variance components by maximizing the evidence')
-    model.fit(n_iter=n_iter)
+    if params_fpath is None:
+        log.write('Estimate hyperparameters by maximizing the evidence')
+        model.fit(n_iter=n_iter)
+    else:
+        log.write('Load hyperparameters from {}'.format(params_fpath))
+        model.load(params_fpath)
 
-    # Output file prefix    
-    prefix = '.'.join(out_fpath.split('.')[:-1])
-    
     # Write output parameters
-    if hasattr(kernel, 'p'):
-        fpath = '{}.p.csv'.format(prefix)
-        log.write('Writing inferred p to {}'.format(fpath))
-        ps = pd.DataFrame(model.to_numpy(kernel.p), columns=np.append(alleles, '*'))
-        ps.to_csv(fpath)
+    fpath = '{}.model_params.pth'.format(out_fpath)
+    log.write('Storing model parameteres at {}'.format(fpath))
+    model.save(fpath)
     
-    if hasattr(kernel, 'lambdas'):
-        fpath = '{}.lambdas.txt'.format(prefix)
-        log.write('Writing inferred lambdas to {}'.format(fpath))
-        with open(fpath, 'w') as fhand:
-            for l in model.to_numpy(kernel.lambdas):
-                fhand.write('{}\n'.format(l))
-    
-    if hasattr(kernel, 'theta'):
-        fpath = '{}.theta.txt'.format(prefix)
-        log.write('Writing inferred theta to {}'.format(fpath))
-        theta = pd.DataFrame(model.to_numpy(kernel.theta))
-        theta.to_csv(fpath)
-    
-    if hasattr(kernel, 'rho'):
-        fpath = '{}.rho.txt'.format(prefix)
-        log.write('Writing inferred rho to {}'.format(fpath))
-        rho = pd.DataFrame(model.to_numpy(kernel.rho))
-        rho.to_csv(fpath)
-    
-    if hasattr(kernel, 'beta'):
-        fpath = '{}.beta.txt'.format(prefix)
-        log.write('Writing inferred beta to {}'.format(fpath))
-        if dummy_allele:
-            alleles = np.append(alleles, '*')
-            
-        beta = pd.DataFrame(model.to_numpy(kernel.beta), columns=alleles)
-        beta.to_csv(fpath)
-    
-    # Predict phenotype in new sequences 
-    if pred_seqs.shape[0] > 0:
-        log.write('Obtain phenotypic predictions')
-        Xpred = seq_to_one_hot(pred_seqs, alleles=alleles)
-        result = pd.DataFrame({'ypred': model.to_numpy(model.predict(Xpred))},
-                              index=pred_seqs)
+    # Predict phenotype in new sequences
+    if test_seqs.shape[0] > 0:
+        log.write('Obtain phenotypic predictions for test data')
+        X_test = seq_to_one_hot(test_seqs, alleles=alleles)
+        y_test = model.to_numpy(model.predict(X_test))
+        result = pd.DataFrame({'y_pred': y_test}, index=test_seqs)
         log.write('\tWriting predictions to {}'.format(out_fpath))
         result.to_csv(out_fpath)
 
     # Write execution time for tracking performance
-    fpath = '{}.time.txt'.format(prefix)
+    fpath = '{}.time.txt'.format(out_fpath)
     log.write('Writing execution times to {}'.format(fpath))        
     with open(fpath, 'w') as fhand:
         fhand.write('fit,{}\n'.format(model.fit_time))
@@ -245,9 +175,9 @@ def main():
             
     # Save plot with training history
     if hasattr(model, 'loss_history'):
-        fpath = '{}.training.png'.format(prefix)
+        fpath = '{}.training.png'.format(out_fpath)
         log.write('Saving plot with loss history to {}'.format(fpath))
-        plot_training_history(model.loss_history, fpath)
+        plot_training_history(np.array(model.loss_history), fpath)
     
     log.finish()
 
