keops module no longer necessary as we have a single class that runs in both backends. Fixes in RhoPi kernel to remove parameters from linearOperators for which the gradient does not seem to propagate during training. DiagonalLinearOperator used to solve the normalization issue in the keops backend

bin/EpiK.py
@@ -45,13 +45,14 @@ def select_kernel(kernel, n_alleles, seq_length, dtype, P, add_het, use_keops):
                                  use_keops=use_keops)
 
         # VC-like kernels
-        elif kernel == 'VC':
-            kernel = VarianceComponentKernel(n_alleles, seq_length, dtype=dtype)
         elif kernel == 'Additive':
             kernel = AdditiveKernel(n_alleles, seq_length, dtype=dtype)
         elif kernel == 'DP':
-            kernel = DeltaPKernel(n_alleles, seq_length, P=P, dtype=dtype)
-            
+            kernel = DeltaPKernel(n_alleles, seq_length, P=P, dtype=dtype,
+                                  use_keops=use_keops)
+        elif kernel == 'VC':
+            kernel = VarianceComponentKernel(n_alleles, seq_length, dtype=dtype,
+                                             use_keops=use_keops)
         else:
             msg = 'Unknown kernel provided: {}'.format(kernel)
             raise ValueError(msg)
@@ -152,6 +153,7 @@ def main():
     n_alleles = alleles.shape[0]
     X = seq_to_one_hot(seqs, alleles=alleles)
     y = data.values[:, 0]
+    log_var0 = np.log(np.var(y) * 0.2)
     if data.shape[1] > 1:
         y_var = data.values[:, 1]
         y_var[y_var < 0.0001] = 0.0001

epik/src/kernel/haploid.py
@@ -1,11 +1,13 @@
 import numpy as np
 import torch as torch
 
+from functools import partial
 from itertools import combinations
 from scipy.special._basic import comb
 from torch.nn import Parameter
 from pykeops.torch import LazyTensor
-from linear_operator.operators import KernelLinearOperator
+from linear_operator.operators import (KernelLinearOperator, MatmulLinearOperator,
+                                       DiagLinearOperator)
 
 from epik.src.utils import get_tensor, log1mexp
 from epik.src.kernel.base import SequenceKernel
@@ -14,19 +16,33 @@ from epik.src.priors import (LambdasFlatPrior, LambdasDeltaPrior, RhosPrior,
 
 
 class AdditiveKernel(SequenceKernel):
-    def __init__(self, n_alleles, seq_length, log_lambdas0=None, **kwargs):
+    def __init__(self, n_alleles, seq_length,
+                 log_lambdas0=None, log_var0=None,
+                 **kwargs):
         super().__init__(n_alleles, seq_length, **kwargs)
         self.log_lambdas0 = log_lambdas0
+        self.log_var0 = log_var0
         self.set_params()
     
     def get_matrix(self):
-        m = torch.tensor([[1, -self.l], [0., self.alpha]])
+        m = torch.tensor([[1, -self.l],
+                          [0., self.alpha]])
         return(m)  
     
     def set_params(self):
-        log_lambdas0 = torch.tensor([0, 0.]) if self.log_lambdas0 is None else self.log_lambdas0
+        m = self.get_matrix()
+
+        if self.log_lambdas0 is None: 
+            # log_var0 = 0. if self.log_var0 is None else self.log_var0
+            # var0 = np.exp(log_var0)
+            # coeffs0 = torch.tensor([0.5 * var0, var0 /(2 * self.l)]).to(dtype=self.dtype)
+            # log_lambdas0 = torch.log(torch.linalg.solve(m, coeffs0))
+            log_lambdas0 = torch.tensor([0., 0.], dtype=self.dtype)
+        else:
+            log_lambdas0 = self.log_lambdas0.to(dtype=self.dtype)
+
         params = {'log_lambdas': Parameter(log_lambdas0, requires_grad=True),
-                  'm': Parameter(self.get_matrix(), requires_grad=False)}
+                  'm': Parameter(m, requires_grad=False)}
         self.register_params(params)
     
     def lambdas_to_coeffs(self, lambdas):
@@ -38,8 +54,15 @@ class AdditiveKernel(SequenceKernel):
     
     def forward(self, x1, x2, diag=False, **kwargs):
         coeffs = self.get_coeffs()
-        return(coeffs[0] + coeffs[1] * (x1 @ x2.T))
-
+        # print(self.log_lambdas, coeffs.detach().cpu().numpy())
+        s = MatmulLinearOperator(x1, x2.T)
+        x1_ = torch.ones(size=(x1.shape[0], 1), dtype=coeffs.dtype, device=coeffs.device)
+        x2_ = torch.ones(size=(1, x2.shape[0]), dtype=coeffs.dtype, device=coeffs.device)
+        s0 = MatmulLinearOperator(x1_, x2_)
+        if diag:
+            return(coeffs[0] * x1_ + coeffs[1] * s.diagonal(dim1=-1, dim2=-2))
+        else:
+            return(coeffs[0] *  s0 + coeffs[1] * s)
 
 class _LambdasKernel(SequenceKernel):
     def calc_polynomial_coeffs(self):
@@ -176,7 +199,7 @@ class RhoPiKernel(SequenceKernel):
     def get_logit_rho0(self):
         # Choose rho0 so that correlation at l/2 is 0.1
         shape = (1, 1) if self.common_rho else (self.l, 1)
-        t = np.exp(-2/self.l * np.log(10.))
+        t = np.exp(-2 / self.l * np.log(10.))
         v = np.log((1 - t) / (self.alpha * t))
         logit_rho0 = torch.full(shape, v, dtype=self.dtype) if self.logit_rho0 is None else self.logit_rho0 
         return(logit_rho0)
@@ -220,7 +243,12 @@ class RhoPiKernel(SequenceKernel):
         if self.common_rho:
             constant *= self.l
         constant += self.log_var
-        
+        # print('Params')
+        # print('log_var', self.log_var.detach().cpu().numpy(), self.train_var)
+        # print('logit_rho', self.logit_rho.detach().cpu().numpy())
+        # print('log_p', self.log_p.detach().cpu().numpy())
+        # print('factors', factors.detach().cpu().numpy())
+        # input()
         return(constant, factors, log_one_p_eta_rho)
     
     def _nonkeops_forward(self, x1, x2, diag=False, **params):
@@ -228,32 +256,33 @@ class RhoPiKernel(SequenceKernel):
         factors = factors.reshape(1, self.t)
         log_one_p_eta_rho = log_one_p_eta_rho.reshape(self.t, 1)
         
+        log_kernel = constant + x1 @ (x2 * factors).T
         if self.correlation:
             log_sd1 = 0.5 * (x1 @ log_one_p_eta_rho)
             log_sd2 = 0.5 * (x2 @ log_one_p_eta_rho).reshape((1, x2.shape[0]))
-            kernel = torch.exp(constant + x1 @ (x2 * factors).T - log_sd1 - log_sd2)
-        else:
-            kernel = torch.exp(constant + x1 @ (x2 * factors).T)
-        # print(kernel)
+            log_kernel = log_kernel - log_sd1 - log_sd2
+        
+        kernel = torch.exp(log_kernel)
         return(kernel)
     
     def _covar_func(self, x1, x2, **kwargs):
         x1_ = LazyTensor(x1[..., :, None, :])
         x2_ = LazyTensor(x2[..., None, :, :])
-        kernel = ((x1_ * x2_).sum(-1)).exp()
+        kernel = (x1_ * x2_).sum(-1).exp()
         return(kernel)
     
     def _keops_forward(self, x1, x2, **kwargs):
         constant, factors, log_one_p_eta_rho = self.get_factors()
-        c = torch.exp(constant)
         f = factors.reshape(1, self.t)
         log_one_p_eta_rho = log_one_p_eta_rho.reshape(1, self.t)
+        c = torch.exp(constant)
         
         kernel = c * KernelLinearOperator(x1, x2 * f, covar_func=self._covar_func, **kwargs)
         if self.correlation:
-            sd1 = torch.exp(0.5 * (x1 * log_one_p_eta_rho).sum(1)).reshape((x1.shape[0], 1))
-            sd2 = torch.exp(0.5 * (x2 * log_one_p_eta_rho).sum(1)).reshape((1, x2.shape[0]))
-            kernel = (kernel / sd1) / sd2
+            sd1_inv_D = DiagLinearOperator(torch.exp(-0.5 * (x1 * log_one_p_eta_rho).sum(1)))
+            sd2_inv_D = DiagLinearOperator(torch.exp(-0.5 * (x2 * log_one_p_eta_rho).sum(1)))
+            kernel = sd1_inv_D @ kernel @ sd2_inv_D
+
         return(kernel)
     
     def get_params(self):

epik/test/test_kernel.py
@@ -106,32 +106,39 @@ class KernelsTests(unittest.TestCase):
         logit_rho0 = torch.tensor([0.])
         kernel = RhoKernel(n_alleles=a, seq_length=l, logit_rho0=logit_rho0)
         x = get_full_space_one_hot(l, a)
-        cov = kernel.forward(x, x)
+        cov = kernel.forward(x, x).detach().numpy()
         assert(cov[0, 0] == 1.5)
         assert(cov[0, 1] == 0.5)
+        
+        I = torch.eye(a ** l)
+        cov2 = (kernel._keops_forward(x, x) @ I).detach().numpy()
+        assert(np.allclose(cov2, cov))
         
         l, a = 2, 2
         logit_rho0 = torch.tensor([[0.], [0.]])
         rho = np.exp(logit_rho0.numpy()) / (1 + np.exp(logit_rho0.numpy()))
         kernel = RhoKernel(n_alleles=a, seq_length=l, logit_rho0=logit_rho0)
         x = get_full_space_one_hot(l, a)
-        cov = kernel.forward(x, x).detach().numpy()[0, :]
-        assert(np.allclose(cov, [1.5 ** 2, 1.5 * 0.5, 1.5 * 0.5, 0.5 ** 2]))
+        cov = kernel.forward(x, x).detach().numpy()
+        assert(np.allclose(cov[0, :], [1.5 ** 2, 1.5 * 0.5, 1.5 * 0.5, 0.5 ** 2]))
+
+        I = torch.eye(a ** l)
+        cov2 = (kernel._keops_forward(x, x) @ I).detach().numpy()
+        assert(np.allclose(cov, cov))
 
         logit_rho0 = torch.tensor([[0.], [-np.log(3)]], dtype=torch.float32)
         rho = (np.exp(logit_rho0.numpy()) / (1 + np.exp(logit_rho0.numpy()))).flatten()
         kernel = RhoKernel(n_alleles=a, seq_length=l, logit_rho0=logit_rho0)
-        cov = kernel.forward(x, x).detach().numpy()[0, :]
+        cov = kernel.forward(x, x).detach().numpy()
         expected = np.array([(1 + rho[0]) * (1 + rho[1]),
                              (1 - rho[0]) * (1 + rho[1]),
                              (1 + rho[0]) * (1 - rho[1]),
                              (1 - rho[0]) * (1 - rho[1])])
-        assert(np.allclose(cov, expected))
+        assert(np.allclose(cov[0, :], expected))
         
-        # Test with KeOps backend
         I = torch.eye(a ** l)
-        cov = (kernel._keops_forward(x, x) @ I).detach().numpy()[0, :]
-        assert(np.allclose(cov, expected))
+        cov2 = (kernel._keops_forward(x, x) @ I).detach().numpy()
+        assert(np.allclose(cov2, cov))
     
     def test_rho_pi_kernel(self):
         l, a = 1, 2
@@ -144,6 +151,10 @@ class KernelsTests(unittest.TestCase):
         expected = np.array([[3, 0.5],
                              [0.5, 1.125]])
         assert(np.allclose(cov, expected))
+
+        I = torch.eye(a ** l)
+        cov2 = (kernel._keops_forward(x, x) @ I).detach().numpy()
+        assert(np.allclose(cov2, cov))
         
         l, a = 2, 2
         logit_rho0 = torch.tensor([[0.],
@@ -156,17 +167,16 @@ class KernelsTests(unittest.TestCase):
         rho = np.array([0.5, 0.5])
         eta = np.array([[4., 0.25],
                         [1., 1.  ]])
-        cov = kernel.forward(x, x).detach().numpy()[0, :]
+        cov = kernel.forward(x, x).detach().numpy()
         expected = np.array([(1 + rho[0] * eta[0, 0]) * (1 + rho[1] * eta[1, 0]),
                              (1 - rho[0])             * (1 + rho[1] * eta[1, 0]),
                              (1 + rho[0] * eta[0, 0]) * (1 - rho[1]),
                              (1 - rho[0])             * (1 - rho[1])])
-        assert(np.allclose(cov, expected))
-        
-        # Test with KeOps backend
+        assert(np.allclose(cov[0, :], expected))
+
         I = torch.eye(a ** l)
-        cov = (kernel._keops_forward(x, x) @ I).detach().numpy()[0, :]
-        assert(np.allclose(cov, expected))
+        cov2 = (kernel._keops_forward(x, x) @ I).detach().numpy()
+        assert(np.allclose(cov2, cov))
         
     def test_ARD_kernel(self):
         l, a = 1, 2
@@ -179,6 +189,10 @@ class KernelsTests(unittest.TestCase):
         expected = np.array([[1, 0.5 / np.sqrt(3 * 1.125)],
                              [0.5 / np.sqrt(3 * 1.125), 1]])
         assert(np.allclose(cov, expected))
+
+        I = torch.eye(a ** l)
+        cov2 = (kernel._keops_forward(x, x) @ I).detach().numpy()
+        assert(np.allclose(cov2, cov))
         
         l, a = 2, 2
         logit_rho0 = torch.tensor([[0.],
@@ -191,18 +205,17 @@ class KernelsTests(unittest.TestCase):
         rho = np.array([0.5, 0.5])
         eta = np.array([[4., 0.25],
                         [1., 1.  ]])
-        cov = kernel.forward(x, x).detach().numpy()[0, :]
+        cov = kernel.forward(x, x).detach().numpy()
         expected = np.array([1, 
                              (1 - rho[0]) / np.sqrt((1 + rho[0] * eta[0, 0]) * (1 + rho[0] * eta[0, 1])),
                              (1 - rho[1]) / np.sqrt((1 + rho[1] * eta[1, 0]) * (1 + rho[1] * eta[1, 1])),
                              np.nan])
         expected[3] = expected[1] * expected[2]
-        assert(np.allclose(cov, expected))
+        assert(np.allclose(cov[0, :], expected))
         
-        # Test with KeOps backend
         I = torch.eye(a ** l)
-        cov = (kernel._keops_forward(x, x) @ I).detach().numpy()[0, :]
-        assert(np.allclose(cov, expected))
+        cov2 = (kernel._keops_forward(x, x) @ I).detach().numpy()
+        assert(np.allclose(cov2, cov))
         
     def test_heteroskedastic_kernel(self):
         l, a = 1, 2

epik/test/test_model.py
@@ -11,18 +11,15 @@ from subprocess import check_call
 from tempfile import NamedTemporaryFile
 
 from scipy.stats import pearsonr
+from gpytorch.kernels import ScaleKernel
+from gpytorch.kernels import RBFKernel as GPRBFKernel
 
 from epik.src.settings import TEST_DATA_DIR, BIN_DIR
 from epik.src.utils import (seq_to_one_hot, get_tensor, split_training_test,
                             get_full_space_one_hot, one_hot_to_seq)
 from epik.src.model import EpiK
-from epik.src.kernel.haploid import VarianceComponentKernel, AdditiveKernel
-from epik.src.priors import (LambdasExpDecayPrior, AllelesProbPrior,
-                             LambdasDeltaPrior, LambdasFlatPrior, RhosPrior)
-from epik.src.kernel.keops import VarianceComponentKernel
-from epik.src.kernel.haploid import RhoKernel, RBFKernel, ARDKernel
-from gpytorch.kernels import ScaleKernel
-from gpytorch.kernels import RBFKernel as GPRBFKernel
+from epik.src.kernel.haploid import (VarianceComponentKernel, AdditiveKernel,
+                                     RhoKernel, RBFKernel, ARDKernel)
 
 
 def get_smn1_data(n, seed=0, dtype=None):
@@ -216,9 +213,10 @@ class ModelsTests(unittest.TestCase):
         X, y, y_var = seq_to_one_hot(data.X.values, alleles=['A', 'B']), data.y.values, data.y_var.values
         train_x, train_y, test_x, test_y, train_y_var = split_training_test(X, y, y_var=y_var, ptrain=ptrain)
 
+        kernel = AdditiveKernel(alpha, l)
         # kernel = RBFKernel(alpha, l)
         # kernel = RhoKernel(alpha, l)
-        kernel = ScaleKernel(ARDKernel(alpha, l))
+        # kernel = ARDKernel(alpha, l)
         # kernel = ScaleKernel(GPRBFKernel())
         # kernel = ScaleKernel(GPRBFKernel(ard_num_dims=train_x.shape[1]))
 