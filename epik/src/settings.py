d8e32e7a067a13e19e00c736bcf362c12a0e6df6
Carlos Marti <carlos199m1@gmail.com>
2024-08-19 15:24:58 -0400

    list of available kernel labels added as a global variable

--- epik/src/settings.py
+++ epik/src/settings.py
@@ -5,3 +5,7 @@ VERSION = '0.1.0'
 BASE_DIR = abspath(join(dirname(__file__), '..'))
 BIN_DIR = join(BASE_DIR, '..', 'bin')
 TEST_DATA_DIR = join(BASE_DIR, 'test', 'data')
+
+KERNELS = ['Additive', 'Pairwise', 'VC', 
+           'Exponential', 'Connectedness', 'Jenga', 
+           'GeneralProduct']
